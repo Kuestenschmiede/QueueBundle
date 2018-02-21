89b8de553d778d8fedbaa3a3dadd08e3a0286009
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2018-02-21 17:46:28 +0100

    copyright 2018

--- Classes/Events/QueueSaveJobResultEvent.php
+++ Classes/Events/QueueSaveJobResultEvent.php
@@ -4,7 +4,7 @@
  * @version   2.0.0
  * @package   con4gis
  * @author    con4gis authors (see "authors.txt")
- * @copyright Küstenschmiede GmbH Software & Design 2016 - 2017.
+ * @copyright Küstenschmiede GmbH Software & Design 2011 - 2018
  * @link      https://www.kuestenschmiede.de
  */
 namespace con4gis\QueueBundle\Classes\Events;
