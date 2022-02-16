f06690a6931b7494d9df26f9ce07ea1e8ae31b60
coastforge-rro <raphael.ross@kuestenschmiede.gmbh>
2022-02-16 16:33:31 +0100

    Change deprecated parent class

--- src/Classes/Events/LoadQueueEvent.php
+++ src/Classes/Events/LoadQueueEvent.php
@@ -11,7 +11,7 @@
 namespace con4gis\QueueBundle\Classes\Events;
 
 use Database\Result;
-use Symfony\Component\EventDispatcher\Event;
+use Symfony\Contracts\EventDispatcher\Event;
 
 /**
  * Class LoadQueueEvent
